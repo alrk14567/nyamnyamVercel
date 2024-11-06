fe0a94d8f86305c1177d8e27c75f8ac883dae2df
alrk14567 <cdutik@gmail.com>
2024-11-06 20:42:22 +0900

    qwer

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export const instance  = axios.create ({
-    baseURL : "https://kidon.nyamnyam.kr"
+    baseURL : "https://abc.nyamnyam.kr"
 }); 
 
 export const instance1  = axios.create ({
